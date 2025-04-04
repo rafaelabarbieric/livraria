03065aaeaa6e038dd7698fab9e5db0a68705751e
rafaelabarbieric <rafaelabarbieric@gmail.com>
2025-04-04 15:02:31 -0300

    feat: criação da API para Editora

--- core/models/editora.py
+++ core/models/editora.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
-    site = models.CharField(max_length=100)
+    site = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.nome}"
